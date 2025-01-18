edb8a339ae434c854d7f4533c26febcff271fe0e
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-01-18 16:16:09 +0530

    Time: 91 ms (86.46%) | Memory: 13.9 MB (23.24%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+// memoization approach
     int countUtil(int i, int j, vector<int>&nums, vector<vector<int>>&dp){
         if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
@@ -17,8 +18,23 @@ public:
         int n = nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        int ans = countUtil(1,n,nums,dp);
-        return ans;
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        // int ans = countUtil(1,n,nums,dp);
+        // return ans;
+        for(int i = n;i>=1;i--){
+            for(int j=i;j<=n;j++){
+                // if(i>j) continue;
+                int maxi = INT_MIN;
+                for(int k=i;k<=j;k++){
+                    int ans = nums[i-1]*nums[k] * nums[j+1] + dp[i][k-1]+ dp[k+1][j];
+                    maxi = max(maxi,ans);
+
+                }
+                dp[i][j]=maxi;
+            }
+        }
+
+        return dp[1][n];
+
     }
 };
