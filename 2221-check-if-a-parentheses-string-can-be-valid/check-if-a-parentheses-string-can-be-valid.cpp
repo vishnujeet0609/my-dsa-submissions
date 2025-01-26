ce7a45309fa28854078e0dd379f7b0a3603c878b
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-01-26 18:23:43 +0530

    Update check-if-a-parentheses-string-can-be-valid.cpp
    added approach to optimze space

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -7,7 +7,7 @@ public:
             return false;
         }
 
-        stack<int> openBrackets, unlocked;
+        stack<int> openBrackets, unlocked; /// we need to optimize this space 
 
         // Iterate through the string to handle '(' and ')'
         for (int i = 0; i < length; i++) {
@@ -40,3 +40,8 @@ public:
         return true;
     }
 };
+
+//we can optimze the space complexity using two iterations in one iteration we will keep track of open brackets and in other closed brackets.
+// locked will act as open in open count iteration and as closed in closed count iteration.
+//if open<0 or close<0 return false;
+// else we will retuen true
