6b0a4390bf36801c41f3e5c0760504083b0a4437
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-01-28 23:34:39 +0530

    Time: 17 ms (33.80%) | Memory: 97.6 MB (35.20%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,77 +1,96 @@
-class Dsu{
-    vector<int>parent;
-    vector<int>size;
+// Approach-1 (Using DSU)
+// T.C : O(m*n * alpha(m*n))
+// S.C : O(m*n)
+class DSU {
+    vector<int> parent;
+    vector<int> size;
+
 public:
-    Dsu(int n){
+    DSU(int n) { //size of parent and size array
         parent.resize(n);
         size.resize(n);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            size[i]=0;
+
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 0; //fis count ios 0 initially for each
         }
     }
-    int find(int x){
-        if(parent[x]==x){
+
+    //find the parent of x
+    int find(int x) {
+        if(parent[x] == x) {
             return x;
         }
-        return parent[x]=find(parent[x]);
+
+        return parent[x] = find(parent[x]); //Path Compression
     }
-    void Union(int x, int y){
+
+    void Union(int x, int y) {
         int x_parent = find(x);
         int y_parent = find(y);
 
-        if(x_parent == y_parent){
+        if(x_parent == y_parent) { //both already in same group
             return;
         }
 
-        if(size[x_parent]>size[y_parent]){
+        if(size[x_parent] > size[y_parent]) {
             parent[y_parent] = x_parent;
-            size[x_parent]+=size[y_parent];
-        }else{
+            size[x_parent] += size[y_parent];
+        } else {
             parent[x_parent] = y_parent;
-            size[y_parent]+=size[x_parent];
+            size[y_parent] += size[x_parent];
         }
     }
-    void setSize(int x, int fishCount){
+
+    void setSize(int x, int fishCount) {
         size[x] = fishCount;
     }
-    int maxFish(){
-        return *max_element(begin(size),end(size));
+
+    int getMaxFishCount() {
+        return *max_element(begin(size), end(size));
     }
 };
 
 class Solution {
 public:
-    vector<vector<int>>directions = {{0,-1},{0,1},{-1,0},{1,0}};
+    vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     int findMaxFish(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
         int totalCells = m*n;
-        Dsu* dsu = new Dsu(totalCells);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]>0){
-                    int idx = i*n + j ;  //using this technique we can convert 2d array element into 1d for use in dsu
-                    dsu->setSize(idx,grid[i][j]);
+
+        DSU dsu(totalCells);
+        //DSU = alpha(totalCells)
+
+        //Initialize size array with initial fish count of each cell
+        //O(m*n)
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] > 0) {
+                    int idx = i*n + j;
+                    dsu.setSize(idx, grid[i][j]);
                 }
             }
         }
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(grid[i][j]>0){
-                    int idx = i * n + j;
-                     for(auto &dir:directions){
+        //O(m*n * alpha(m*n))
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] > 0) {
+                    int idx = i * n + j; //parent ka index milgaya
+                    for(auto &dir : directions) {
                         int i_ = i + dir[0];
                         int j_ = j + dir[1];
-                        if(i_>=0 && j_>=0 && i_<m && j_<n && grid[i_][j_]>0){
-                            int idx_ = i_ * n + j_;
-                            dsu->Union(idx,idx_);
+                        if(i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && grid[i_][j_] > 0) {
+                            int idx_ = i_ * n + j_; //parent ka index
+                            dsu.Union(idx, idx_);
                         }
-                     }
+                    }
                 }
             }
         }
-        return dsu->maxFish();
+
+        return dsu.getMaxFishCount();
+
     }
 };
