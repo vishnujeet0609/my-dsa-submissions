1a1a1b260825fb1ada038df1fc1c041789b0683d
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-01-29 23:30:09 +0530

    Update redundant-connection.cpp

--- february/684-redundant-connection/redundant-connection.cpp
+++ february/684-redundant-connection/redundant-connection.cpp
@@ -1,3 +1,8 @@
+
+//Approach-2 - Using BFS
+//T.C : O(n^2)
+//S.C : O(n)
+
 class DSU{
     vector<int>parent;
     vector<int>rank;
@@ -48,6 +53,61 @@ public:
             }
             dsu->Union(u,v);
         }
+        return {};
+    }
+};
+
+
+
+
+
+
+
+//Approach-2 - Using DFS
+//T.C : O(n^2)
+//S.C : O(n)
+class Solution {
+public:
+
+    bool dfs(unordered_map<int, vector<int>> &adj, int u, int v, vector<bool>& visited) {
+        visited[u] = true;
+
+        if(u == v) {
+            return true;
+        }
+
+        for(int &ngbr : adj[u]) {
+            if(visited[ngbr]) continue;
+
+            if(dfs(adj, ngbr, v, visited)) {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        //number of nodes = n
+        //number of edges = n
+
+        unordered_map<int, vector<int>> adj;
+
+        for(int i = 0; i < n; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            vector<bool> visited(n, false);
+            if(adj.find(u) != adj.end() && adj.find(v) != adj.end() && dfs(adj, u, v, visited)) {
+                return edges[i];
+            }
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
         return {};
     }
 };
