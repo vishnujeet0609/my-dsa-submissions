86a5ff8573e600ffb2ea1dd204bd20cff683a951
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-02-01 22:11:42 +0530

    Update special-array-i.cpp

--- february/3429-special-array-i/special-array-i.cpp
+++ february/3429-special-array-i/special-array-i.cpp
@@ -15,3 +15,20 @@ public:
         return true;
     }
 };
+
+//Approach-2 (Using Bit magic)
+//T.C : O(n) 
+//S.C : O(1)
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i = 0; i <= n-2; i++) {
+            if((nums[i]&1) == (nums[i+1]&1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
