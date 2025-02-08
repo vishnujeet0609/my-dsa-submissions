0f83b30d191ca4ca076b29f3bba067976404e52d
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-02-08 23:09:30 +0530

    Update design-a-number-container-system.cpp

--- february/2434-design-a-number-container-system/design-a-number-container-system.cpp
+++ february/2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -41,3 +41,45 @@ public:
  * obj->change(index,number);
  * int param_2 = obj->find(number);
  */
+
+
+
+//Approach-2 (Using two maps : map of int, int and map of int, min-heap)
+//T.C : O(log(n) + k*logn)
+//S.C : O(n)
+class NumberContainers {
+public:
+    //Space : O(n)
+    unordered_map<int, int> idxToNum;
+    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> numToIdx;
+
+    NumberContainers() {
+        //do nothing    
+    }
+    
+    void change(int index, int number) {
+        idxToNum[index] = number; //O(1)
+
+        numToIdx[number].push(index); //O(log(n))
+    }
+    
+    int find(int number) {
+        if(!numToIdx.count(number)) {
+            return -1;
+        }
+
+        auto& pq = numToIdx[number];
+
+        while(!pq.empty()) { //O(k * logn)
+            int idx = pq.top(); //O(1)
+
+            if(idxToNum[idx] == number) {
+                return idx;
+            }
+
+            pq.pop(); //O(log(n))
+        }
+
+        return -1;
+    }
+};
