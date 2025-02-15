aa19dcaa966db563f4db0973486a2c6ac69e755d
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-02-15 17:06:14 +0530

    Update find-the-punishment-number-of-an-integer.cpp

--- february/2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ february/2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,36 +1,76 @@
+//Approach-1 : (Recursion + Memoization)
+//T.C : O(n * 2^(log10(n^2)))
+//S.C : O(n * log10(n^2))
 class Solution {
 public:
-    bool check(int i, int currSum, string s,int num, vector<vector<int>>&dp){
-        if(i== s.length()){
-            return currSum==num;
+    bool check(int i, int currsum, string s, int num, vector<vector<int>>& t) {
+        if (i == s.length()) {
+            return currsum == num;
         }
-        if(currSum>num){
+        if (currsum > num) 
             return false;
-        }
-        if(dp[i][currSum]!=-1){
-            return dp[i][currSum];
-        }
+
+        if (t[i][currsum] != -1) //we have repeating subproblems. Draw tree diagram of "1296"
+            return t[i][currsum];
+
         bool possible = false;
-        for(int j=i;j<s.length();j++){
-            string sub = s.substr(i,j-i+1);
-            int val = stoi(sub);
-            possible = possible || check(j+1,currSum+val,s,num,dp);
-            if(possible==true){
+        for (int j = i; j < s.length(); j++) {
+            string currentString = s.substr(i, j - i + 1);
+            int addend = stoi(currentString);
+
+            possible = possible || check(j + 1, currsum + addend, s, num, t);
+            if (possible == true) 
                 return true;
-            }
         }
-        return dp[i][currSum]=possible;
+
+        return t[i][currsum] = possible;
     }
+
     int punishmentNumber(int n) {
-        int punish = 0;
-        for(int num=1;num<=n;num++){
-            int sq = num*num;
-            string s = to_string(sq);
-            vector<vector<int>>dp(s.length(),vector<int>(num+1,-1));
-            if(check(0,0,s,num,dp)==true){
-                punish+=sq;
+        int punishmentNum = 0;
+        for (int num = 1; num <= n; num++) {
+            int squareNum = num * num;
+            string s = to_string(squareNum);
+
+            vector<vector<int>> t(s.size(), vector<int>(num + 1, -1));
+
+            if (check(0, 0, s, num, t)) {
+                punishmentNum += squareNum;
             }
         }
-        return punish;
+        return punishmentNum;
     }
 };
+
+
+//Approach-2 : (Recursion + Memoization)
+//T.C : O(n * 2^(log10(n^2)))
+//S.C : O(log10(n^2))
+class Solution {
+public:
+    bool check(int num, int currnum, int target) {
+        if(num == 0) {
+            return currnum == target;
+        }
+        
+        return check(num / 10, currnum + num % 10, target) ||
+               check(num / 100, currnum + num % 100, target) ||
+               check(num / 1000, currnum + num % 1000, target) ||
+               check(num / 10000, currnum + num % 10000, target);
+    }
+
+    int punishmentNumber(int n) {
+        int punishmentNum = 0;
+        for (int currentNum = 1; currentNum <= n; currentNum++) {
+            int squareNum = currentNum * currentNum;
+            if (check(squareNum, 0, currentNum)) {                                                                                        
+                punishmentNum += squareNum;
+            }
+        }
+        return punishmentNum;
+    }
+};
+
+
+
+
