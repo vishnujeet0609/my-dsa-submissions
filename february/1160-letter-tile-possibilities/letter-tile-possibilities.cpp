0fcc0186fcad340b38028001fb7a65d4a0991d0f
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-02-17 21:40:53 +0530

    Update letter-tile-possibilities.cpp

--- february/1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ february/1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -33,3 +33,39 @@ public:
         return result.size()-1;
     }
 };
+
+
+
+//Approach-2 (Using count of characters + backtracking)
+//T.C : O(n!)
+//S.C : O(n), total possible sequences = n! and each having n length
+class Solution {
+public:
+    int total;
+    void findSequences(vector<int>& count) {
+        total++;
+
+        for (int pos = 0; pos < 26; pos++) {
+            if (count[pos] == 0) {
+                continue;
+            }
+
+            count[pos]--;
+            findSequences(count);
+            count[pos]++;
+        }
+    }
+
+    int numTilePossibilities(std::string tiles) {
+        total = 0;
+
+        vector<int> count(26, 0);
+        for (char c : tiles) {
+            count[c - 'A']++;
+        }
+
+        findSequences(count);
+        return total-1;
+    }
+};
+
