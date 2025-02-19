2fd2129cac4ab2d5b05e6612030785f6d754c6f3
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-02-19 21:29:08 +0530

    Update check-completeness-of-a-binary-tree.cpp

--- february/998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ february/998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -9,30 +9,65 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Approach-1 : BFS
 class Solution {
 public:
-    int countTotalNodes(TreeNode* root){
-        if(!root) return 0;
-        return 1 + countTotalNodes(root->left) + countTotalNodes(root->right);
-    }
-
-    bool check(TreeNode* root,int i,int &cnt){
-       if(!root) return true;
-        
-        if(cnt<i) return false;
-       
-        
-        return check(root->left,2 * i,cnt) && check(root->right,2*i +1,cnt);;
-        
-    }
     bool isCompleteTree(TreeNode* root) {
-        if(!root){
-            return true;
+        //Using BFS
+        
+        queue<TreeNode*> que;
+        
+        que.push(root);
+        
+        bool past = false; //kya aapne past me NULL node dekha hai ?
+        
+        while(!que.empty()) {
+            TreeNode* node = que.front();
+            que.pop();
+            
+            if(node == NULL) {
+                past = true;
+            } else {
+                if(past == true) {
+                    return false;
+                }
+                
+                que.push(node->left);
+                que.push(node->right);
+            }
         }
+        
+        return true;
+    }
+};
+
+
+//Approach-2 : DFS
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+    
+    bool dfs(TreeNode* root, int i, int totalNodes) {
+        if(root == NULL)
+            return true;
+        
+        if(i > totalNodes)
+            return false;
+        
+        return dfs(root->left, 2*i, totalNodes) && 
+               dfs(root->right, 2*i + 1, totalNodes);
+    }
+    
+    //Using DFS
+    bool isCompleteTree(TreeNode* root) {
+        int totalNodes = countNodes(root);
+        
         int i = 1;
-        int cnt =countTotalNodes(root);
-        check(root,i,cnt);
-       
-        return check(root,i,cnt);
+        return dfs(root, i, totalNodes);
     }
 };
