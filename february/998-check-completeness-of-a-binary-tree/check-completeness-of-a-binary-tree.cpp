b9b8067d3901ccea739e0af6a6df9f32c6ee9db8
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-02-19 21:27:14 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (98.20%) - LeetSync

--- february/998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ february/998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -11,26 +11,28 @@
  */
 class Solution {
 public:
+    int countTotalNodes(TreeNode* root){
+        if(!root) return 0;
+        return 1 + countTotalNodes(root->left) + countTotalNodes(root->right);
+    }
+
+    bool check(TreeNode* root,int i,int &cnt){
+       if(!root) return true;
+        
+        if(cnt<i) return false;
+       
+        
+        return check(root->left,2 * i,cnt) && check(root->right,2*i +1,cnt);;
+        
+    }
     bool isCompleteTree(TreeNode* root) {
         if(!root){
             return true;
         }
-        queue<TreeNode*>que;
-        bool past = false;
-        que.push(root);
-        while(!que.empty()){
-            TreeNode* node = que.front();
-            que.pop();
-            if(node==NULL){
-                past=true;
-                continue;
-            }
-            if(node && past){
-                return false;
-            }
-            que.push(node->left);
-            que.push(node->right);
-        }
-        return true;
+        int i = 1;
+        int cnt =countTotalNodes(root);
+        check(root,i,cnt);
+       
+        return check(root,i,cnt);
     }
 };
