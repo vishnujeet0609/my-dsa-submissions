9adf50cff06b44b849849094668c7c245c4b010c
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-02-21 19:36:39 +0530

    Update find-elements-in-a-contaminated-binary-tree.cpp

--- february/1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
+++ february/1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
@@ -9,40 +9,72 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Approach-1 (Using DFS)
+//T.C : O(n)
+//S.C : O(n)
 class FindElements {
 public:
-    unordered_set<int>st;
-    void Bfs(TreeNode* root, int x){
-        queue<TreeNode*> que;
+    unordered_set<int> st;
+    void dfs(TreeNode* root, int x) {
+        if(!root)
+            return;
+        
         root->val = x;
-        que.push(root);
-        while(!que.empty()){
-            TreeNode* temp = que.front();
-            que.pop();
-            st.insert(temp->val);
-            if(temp->left){
-                temp->left->val = 2*temp->val+1;
-                que.push(temp->left);
-            }
-              if(temp->right){
-                temp->right->val = 2*temp->val+2;
-                que.push(temp->right);
-            }
-        }
-    
+        st.insert(x);
+        dfs(root->left, 2*x+1);
+        dfs(root->right, 2*x+2);
     }
     FindElements(TreeNode* root) {
         st.clear();
-        Bfs(root,0);
+        dfs(root, 0);
     }
     
     bool find(int target) {
-        if(st.count(target)){
-            return true;
-        }
-        return false;
+        return st.count(target);
     }
 };
+
+
+//Approach-2 (Using BFS)
+//T.C : O(n)
+//S.C : O(n)
+class FindElements {
+public:
+    unordered_set<int> st;
+    void bfs(TreeNode* root, int x) {
+        queue<TreeNode*> que;
+        root->val = x;
+        que.push(root);
+
+        while(!que.empty()) {
+            TreeNode* temp = que.front();
+            que.pop();
+
+            st.insert(temp->val);
+
+            if(temp->left) {
+                temp->left->val = 2*temp->val + 1;
+                que.push(temp->left);
+            }
+
+            if(temp->right) {
+                temp->right->val = 2*temp->val + 2;
+                que.push(temp->right);
+            }
+            
+        }
+    }
+    FindElements(TreeNode* root) {
+        st.clear();
+        bfs(root, 0);
+    }
+    
+    bool find(int target) {
+        return st.count(target);
+    }
+};
+
+
 
 /**
  * Your FindElements object will be instantiated and called as such:
