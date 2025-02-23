2f174cc9c30b6b205a3f9243ac2aae82434893d8
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-02-23 23:05:51 +0530

    Update construct-binary-tree-from-preorder-and-postorder-traversal.cpp

--- february/925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ february/925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -9,29 +9,78 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Approach-1 (brute force)
+//T.C : O(n^2)
+//S.C : O(n) for System Stack used for Recursion
 class Solution {
 public:
-    int n;
-    TreeNode* solve(int preStart,int postStart,int preEnd,vector<int>& preorder, vector<int>&postorder){
-        if(preStart>preEnd){
+    TreeNode* solve(int prestart, int poststart, int preend, vector<int>& preorder, vector<int>& postorder) {
+        if(prestart > preend) {
             return NULL;
         }
-         TreeNode* root = new TreeNode(preorder[preStart]);
-        if(preStart==preEnd){
+
+        TreeNode* root = new TreeNode(preorder[prestart]);
+        if(prestart == preend) {
             return root;
         }
-        int nextNode = preorder[preStart+1];
-        int j = postStart;
-        while(j<n && postorder[j]!=nextNode){
+        int nextNode = preorder[prestart+1]; //root of left subtree
+
+        int j = poststart;
+        while(postorder[j] != nextNode) {
             j++;
         }
-    int subtreeLen = j-postStart+1;
-    root->left = solve(preStart+1,postStart,preStart+subtreeLen,preorder,postorder);
-    root->right = solve(preStart+subtreeLen+1,j+1,preEnd,preorder,postorder);
-    return root;
+
+        int num = j - poststart + 1;
+
+        root->left = solve(prestart+1, poststart, prestart+num, preorder, postorder);
+        root->right = solve(prestart+num+1, j+1, preend, preorder, postorder);
+
+        return root;
     }
+
+
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
-        n = preorder.size();
-        return solve(0,0,n-1,preorder,postorder);
+        int n = preorder.size();
+        return solve(0, 0, n-1, preorder, postorder);
+    }
+};
+
+
+
+//Approach-2 (using map to optimise)
+//T.C : O(n)
+//S.C : O(n) using map of size n (you can also include n for System Stack used for Recursion)
+class Solution {
+public:
+    TreeNode* solve(int prestart, int poststart, int preend, vector<int>& preorder, vector<int>& postorder,
+                    unordered_map<int, int>& mp) {
+        if(prestart > preend) {
+            return NULL;
+        }
+
+        TreeNode* root = new TreeNode(preorder[prestart]);
+        if(prestart == preend) {
+            return root;
+        }
+        int nextNode = preorder[prestart+1]; //root of left subtree
+
+        int j = mp[nextNode];
+
+        int num = j - poststart + 1;
+
+        root->left = solve(prestart+1, poststart, prestart+num, preorder, postorder, mp);
+        root->right = solve(prestart+num+1, j+1, preend, preorder, postorder, mp);
+
+        return root;
+    }
+
+
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        int n = preorder.size();
+        unordered_map<int, int> mp;
+        for(int i = 0; i < n; i++) {
+            mp[postorder[i]] = i;
+        }
+        return solve(0, 0, n-1, preorder, postorder, mp);
     }
 };
