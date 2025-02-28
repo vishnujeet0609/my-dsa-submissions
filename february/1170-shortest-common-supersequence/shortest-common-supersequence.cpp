ab18e2d155c2fb2d6fd132c707173e2ba84f6a1a
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-02-28 08:01:53 +0530

    Time: 27 ms (38.39%) | Memory: 26.7 MB (87.72%) - LeetSync

---
+++ february/1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string word1, string word2) {
+        int n= word1.length();
+        int m = word2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+         for(int i=0;i<=m;i++){
+            dp[0][i]=0;
+        }
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        int i= n;
+        int j = m;
+        int len = dp[n][m];
+        // int index = len-1;
+        string ans= "";
+        while(i>0 && j>0){
+            if(word1[i-1]==word2[j-1]){
+                ans+=word1[i-1];
+                // index--;
+                i--;
+                j--;
+            }else{
+                if(dp[i-1][j]>dp[i][j-1]){
+                    ans+=word1[i-1];
+                    i--;
+                }else{
+                    ans+=word2[j-1];
+                    j--;
+                }
+            }
+        }
+        while(i>0){
+            ans+=word1[i-1];
+            i--;
+        }
+        while(j>0){
+            ans+=word2[j-1];
+            j--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
