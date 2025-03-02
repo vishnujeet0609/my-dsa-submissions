667c79cfe3e32989a3893350a9a70e5501a42812
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-03-02 16:11:29 +0530

    Update word-search.cpp

--- march/79-word-search/word-search.cpp
+++ march/79-word-search/word-search.cpp
@@ -1,43 +1,51 @@
+/*Space Complexity: O(L)
+Time Complexity: O(M * N * 3^L)
+Space Complexity is because of recursion - to store function stack context.
+Time Complexity - from every block we go in three adjacent blocks (avoiding the direction we came from). 
+This walk can go for max of L times. So each thred at most goes L length long. -> O(3^L).
+Now this is applied at each node from main calling function -> O(M * N). Therefore, O(M * N * 3^L).
+*/
 class Solution {
 public:
-    int n,m,l;
-    vector<vector<int>>directions = {{0,1},{0,-1},{1,0},{-1,0}};
-    bool find(vector<vector<char>>& board, int i,int j,int idx, string& word){
-        if(idx >= l){
+    int l, m, n;
+    vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    bool find(vector<vector<char>>& board, int i, int j, string &word, int idx) {
+        if(idx >= l)
             return true;
-        }
-        if(i<0 || i>=m || j<0 || j>=n || board[i][j]!=word[idx]){
+        
+        if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[idx])
             return false;
-        }
+        
         char temp = board[i][j];
-        board[i][j]='$';
-        for(vector<int>& dir: directions){
-            int i_ =  i + dir[0];
-            int j_ =  j + dir[1];
-            if(find(board,i_,j_,idx+1,word)){
+        board[i][j] = '$';
+        
+        for(auto& dir : directions) {
+            int i_ = i + dir[0];
+            int j_ = j + dir[1];
+            
+            if(find(board, i_, j_, word, idx+1))
                 return true;
-            }
-
         }
+        
         board[i][j] = temp;
         return false;
-
     }
-
+    
     bool exist(vector<vector<char>>& board, string word) {
         m = board.size();
         n = board[0].size();
         l = word.length();
-        if(m*n < l){
+        if(m*n < l)
             return false;
-        }
-        for(int i = 0;i<m;i++){
-            for(int j=0; j<n;j++){
-                if(board[i][j]==word[0] && find(board,i,j,0,word)){
+        
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(board[i][j] == word[0] && find(board, i, j, word, 0)) {
                     return true;
                 }
             }
         }
+        
         return false;
     }
 };
