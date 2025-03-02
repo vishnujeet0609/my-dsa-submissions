4f3d9114e5801135393382c9c7f746abce6b8efb
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-03-02 17:03:03 +0530

    Update unique-paths-iii.cpp

--- march/1022-unique-paths-iii/unique-paths-iii.cpp
+++ march/1022-unique-paths-iii/unique-paths-iii.cpp
@@ -1,47 +1,60 @@
+
 class Solution {
 public:
-    int n,m;
-    int emptycells;
+    int m, n;
+    int emptyCells;
     int result = 0;
-    vector<vector<int>>directions = {{0,1},{0,-1},{1,0},{-1,0}};
-    void find(int i, int j, int currcells, vector<vector<int>>& grid){
-        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]==-1){
+    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
+    void dfs(vector<vector<int>>& grid, int curr_count, int i, int j) {
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == -1) {
             return;
         }
-        if(grid[i][j]==2){
-            if(currcells==emptycells)
+        
+        if(grid[i][j] == 2) {
+            if(curr_count == emptyCells) {
                 result++;
+            }
             return;
         }
-        grid[i][j]=-1;
-        for(auto& dir: directions){
+        
+        
+        grid[i][j] = -1;
+        for(vector<int> dir:directions) {
             int i_ = i + dir[0];
             int j_ = j + dir[1];
-            find(i_,j_,currcells+1,grid);
+            dfs(grid, curr_count+1, i_, j_);
         }
-        grid[i][j]=0;
+        grid[i][j] = 0;
     }
-
+    
     int uniquePathsIII(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
-        int start_x;
-        int start_y;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==0){
-                    emptycells++;
-                }
-                if(grid[i][j]==1){
+        m = grid.size();
+        n = grid[0].size();
+        emptyCells = 0;
+        result = 0;
+        
+        int start_x = 0;
+        int start_y = 0;
+        
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(grid[i][j] == 0)
+                    emptyCells++;
+                
+                if(grid[i][j] == 1) {
                     start_x = i;
                     start_y = j;
                 }
             }
         }
-        emptycells+=1;
-        int currcells = 0;
-        find(start_x,start_y,currcells,grid);
+        
+        emptyCells += 1; //walk over every non-obstacle square exactly once.
+        int curr_count = 0;
+        
+        dfs(grid, curr_count, start_x, start_y);
+        
         return result;
-
     }
 };
+
