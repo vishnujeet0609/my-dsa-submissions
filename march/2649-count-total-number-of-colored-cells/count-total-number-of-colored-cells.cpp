a3d594b4763c5ab6c36dea53fc4f787b940026ae
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-03-05 06:59:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (91.36%) - LeetSync

--- march/2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ march/2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+
+    //1,5,13,25... diff between the numbers is
+    // 4, 8, 12,... 
     long long coloredCells(int n) {
         if(n==1) return 1;
         long long diff  = 1LL* 2*(n-1)*(n-1+1);
