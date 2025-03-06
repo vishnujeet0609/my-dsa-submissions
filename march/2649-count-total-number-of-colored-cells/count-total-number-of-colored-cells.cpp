5b913edb138965b67746b8d146ca89cbe9b01ecd
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-03-06 06:42:27 +0530

    Update count-total-number-of-colored-cells.cpp

--- march/2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ march/2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,11 +1,28 @@
+//Approach-1 (Using simulation)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
-
-    //1,5,13,25... diff between the numbers is
-    // 4, 8, 12,... 
     long long coloredCells(int n) {
-        if(n==1) return 1;
-        long long diff  = 1LL* 2*(n-1)*(n-1+1);
-        long long ans = 1 + diff;
-        return ans;    }
+        int t = 1;
+        long long cells = 1;
+
+        while(t <= n) {
+            cells += (4 * (t-1));
+            t++;
+        }
+
+        return cells;
+    }
+};
+
+
+//Approach-2 (Using Mathematical derivation)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    long long coloredCells(int n) {
+        return 1 + (2 * (long long)(n-1) * n);
+    }
 };
