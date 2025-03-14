3ac4dbf351690947fb95711d2a5e34bf8014e050
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-03-14 20:25:36 +0530

    Time: 27 ms (60.91%) | Memory: 88.3 MB (7.85%) - LeetSync

---
+++ march/1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canDistr(vector<int>& candies,int mid, long long k){
+        for(int i = 0; i<candies.size();i++){
+            k-=candies[i]/mid;
+            if(k<=0) return true;
+        }
+        return k<=0;
+    }
+    int maximumCandies(vector<int>& candies, long long k) {
+        int n = candies.size();
+        long long total = 0;
+        int maxC = 0;
+        for(int i = 0;i<n;i++){
+            total+=candies[i];
+            maxC = max(maxC,candies[i]);
+        }
+        if(total<k){
+            return 0;
+        }
+        int l = 1;
+        int r  = maxC;
+        int result = 0;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(canDistr(candies,mid,k)){
+                result = mid;
+                l = mid + 1;
+                
+            }else{
+                r = mid-1;
+            }
+        }
+      return result;
+    }
+};
