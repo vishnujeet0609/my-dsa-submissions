28bd589d21b97f9c8a08f51572d7578395b998a9
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-03-14 20:27:59 +0530

    Update maximum-candies-allocated-to-k-children.cpp

--- march/1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ march/1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -1,36 +1,89 @@
+//Approach-1 (Brute Force)
+//T.C : O(n * maxC), maxC = maximum candy value
+//S.C : O(1)
 class Solution {
 public:
-    bool canDistr(vector<int>& candies,int mid, long long k){
-        for(int i = 0; i<candies.size();i++){
-            k-=candies[i]/mid;
-            if(k<=0) return true;
-        }
-        return k<=0;
-    }
     int maximumCandies(vector<int>& candies, long long k) {
         int n = candies.size();
-        long long total = 0;
+
         int maxC = 0;
-        for(int i = 0;i<n;i++){
-            total+=candies[i];
-            maxC = max(maxC,candies[i]);
+        int total = 0;
+        for(int i = 0; i < n; i++) {
+            total += candies[i];
+            maxC = max(maxC, candies[i]);
         }
-        if(total<k){
+
+        if(total < k) {
             return 0;
         }
+
+
+        //Brute force try from maxC till 1
+        for(int c = maxC; c >= 1; c--) {
+            long long count = 0;
+
+            for(int i = 0; i < candies.size(); i++) {
+                count += candies[i]/c; //how many children can get c candies from this pile
+            }
+            if(count >= k) {
+                return c;
+            }
+        }
+
+        return 0;
+    }
+};
+
+
+
+//Approach-2 (Binary Search on answer)
+//T.C : O(n * log(maxC)), maxC = maximum candy value
+//S.C : O(1)
+class Solution {
+public:
+    bool canDistr(vector<int>& candies, int mid, long long k) {
+        int n = candies.size();
+
+        for(int i = 0; i < n; i++) {
+            k -= candies[i]/mid;
+
+            if(k <= 0) { //all children got mid candies
+                return true; //Early return
+            }
+        }
+
+        return k <= 0; //all children got the mid candies
+    }
+
+    int maximumCandies(vector<int>& candies, long long k) {
+        int n = candies.size();
+
+        int maxC = 0;
+        long long total = 0;
+        for(int i = 0; i < n; i++) {
+            total += candies[i];
+            maxC = max(maxC, candies[i]);
+        }
+
+        if(total < k) {
+            return 0;
+        }
+
         int l = 1;
-        int r  = maxC;
+        int r = maxC;
         int result = 0;
-        while(l<=r){
+        while(l <= r) {
             int mid = l + (r-l)/2;
-            if(canDistr(candies,mid,k)){
+
+            if(canDistr(candies, mid, k)) {
                 result = mid;
-                l = mid + 1;
-                
-            }else{
+                l = mid+1;
+            } else {
                 r = mid-1;
             }
         }
-      return result;
+
+        return result;
     }
 };
+
