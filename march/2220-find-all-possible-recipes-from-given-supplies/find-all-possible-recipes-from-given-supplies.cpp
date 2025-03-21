1d07c5883a625b96f0faf30f713f0b2495f31611
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-03-21 22:19:37 +0530

    Time: 33 ms (94.76%) | Memory: 101.3 MB (88.72%) - LeetSync

---
+++ march/2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,49 @@
+//Approach-2 - Using Topological Sorting
+//T.C : (n + m + S)
+//S.C : O(n+S)
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        int n = recipes.size();
+        unordered_set<string> st(begin(supplies), end(supplies));
+
+        //adj
+        unordered_map<string, vector<int>> adj; //ing ---> recipe
+
+
+        vector<int> indegree(n, 0);
+        for(int i = 0; i < n; i++) {
+            for(string& ing : ingredients[i]) {
+                if(!st.count(ing)) {
+                    adj[ing].push_back(i);
+                    indegree[i]++;
+                }
+            }
+        }
+
+        queue<int> que;
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                que.push(i);
+            }
+        }
+
+        vector<string> result;
+        while(!que.empty()) {
+            int i = que.front();
+            que.pop();
+            string recipe = recipes[i];
+            result.push_back(recipe);
+
+            for(int &idx : adj[recipe]) {
+                indegree[idx]--;
+                if(indegree[idx] == 0) {
+                    que.push(idx);
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
