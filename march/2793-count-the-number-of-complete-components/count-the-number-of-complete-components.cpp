3bb784ec35424a46c1b6fae2a9ee899c670c24ac
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-03-22 22:19:10 +0530

    Update count-the-number-of-complete-components.cpp

--- march/2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ march/2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,3 +1,109 @@
+// Approach-1 (Using DFS)
+// T.C : O(V+E)
+// S.C : O(V+E)
+class Solution {
+public:
+    void dfs(int i, unordered_map<int, vector<int>>& adj, vector<bool>& visited, int& v, int& e) {
+        visited[i] = true;
+        v++;
+        e += adj[i].size();
+
+        for(int &ngbr : adj[i]) {
+            if(!visited[ngbr]) {
+                dfs(ngbr, adj, visited, v, e);
+            }
+        }
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+
+        int result = 0;
+
+        //Build the graph
+        for(auto &edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<bool> visited(n, false);
+        for(int i = 0; i < n; i++) {
+            if(visited[i] == true) {
+                continue;
+            }
+            int v = 0;
+            int e = 0;
+            dfs(i, adj, visited, v, e);
+
+            if((v*(v-1)) == e) {
+                result++;
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+// Approach-2 (Using BFS)
+// T.C : O(V+E)
+// S.C : O(V+E)
+class Solution {
+public:
+    void bfs(int i, unordered_map<int, vector<int>>& adj, vector<bool>& visited, int& v, int &e) {
+        queue<int> que;
+        que.push(i);
+        visited[i] = true;
+
+        while(!que.empty()) {
+            int curr = que.front();
+            que.pop();
+            v++;
+            e += adj[curr].size();
+
+            for(int &ngbr : adj[curr]) {
+                if(!visited[ngbr]) {
+                    visited[ngbr] = true;
+                    que.push(ngbr);
+                }
+            }
+        }
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+
+        int result = 0;
+
+        //Build the graph
+        for(auto &edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<bool> visited(n, false);
+        for(int i = 0; i < n; i++) {
+            if(visited[i] == true) {
+                continue;
+            }
+            int v = 0;
+            int e = 0;
+            bfs(i, adj, visited, v, e);
+
+            if((v*(v-1)) == e) {
+                result++;
+            }
+        }
+        
+        return result;
+    }
+};
+
+
 // Approach-3 (Using DSU)
 // T.C : O(E * alpha(V))
 // S.C : O(V)
