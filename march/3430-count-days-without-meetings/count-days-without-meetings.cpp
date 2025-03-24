537c9acc3c9228735ca0fe18f723a1c691c377ee
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-03-24 22:23:51 +0530

    Time: 71 ms (53.06%) | Memory: 126.4 MB (80.10%) - LeetSync

---
+++ march/3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -0,0 +1,30 @@
+//Approach (Simple sorting based on starting point)
+//T.C : O(nlogn)
+//S.C : O(1)
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        int n = meetings.size();
+
+        sort(begin(meetings), end(meetings));
+
+        int result = 0;
+        int start = 0;
+        int end = 0;
+
+        for(auto &meet : meetings) {
+            if(meet[0] > end) {
+                result += meet[0] - end - 1;
+            }
+
+            end = max(end, meet[1]);
+        }
+
+        if(days > end) {
+            result += days-end;
+        }
+
+        return result;
+    }
+};
+
