1f5fb0fc873b6f38d11242598a0f47a903373da3
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-04-02 00:01:59 +0530

    Update solving-questions-with-brainpower.cpp

--- march/2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ march/2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -26,3 +26,27 @@ public:
         return solve(0, questions, t);
     }
 };
+
+
+//Approach-2 (Using Bottom Up DP)
+class Solution {
+public:
+
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        
+        if(n == 1)
+            return questions[0][0];
+        
+        vector<long long> t(200001);
+        //t[i] = Max points gained by Questions from questions[i to n-1]
+        //return t[0] - from 0 to n-1
+
+        
+        for(int i = n-1; i >= 0; i--) {
+            t[i] = max(questions[i][0] + t[i + questions[i][1] + 1], t[i+1]);
+        }
+        
+        return t[0];
+    }
+};
