247fb11f747014a2649a4f7c37a930ac226910e8
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-04-04 19:26:05 +0530

    Update lowest-common-ancestor-of-deepest-leaves.cpp

--- april/1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ april/1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -9,6 +9,50 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Approach-1 (Using 2 Pass Solution and LCA code)
+//T.C : O(2*n) ~= O(n)
+//S.C : O(maxDepth) System stack space
+class Solution {
+public:
+    unordered_map<int, int> mp;
+    int maxD = 0;
+
+    TreeNode* LCA(TreeNode* root) {
+        if(root == NULL || mp[root->val] == maxD) {
+            return root;
+        }
+
+        TreeNode* l = LCA(root->left);
+        TreeNode* r = LCA(root->right);
+
+        if(l && r) {
+            return root;
+        }
+
+        return l != NULL ? l : r;
+    }
+
+    void depth(TreeNode* root, int d) {
+        if(!root) {
+            return;
+        }
+
+        maxD = max(maxD, d);
+        mp[root->val] = d;
+        depth(root->left, d+1);
+        depth(root->right, d+1);
+    }
+    //T.C : O(n)
+    //S.C : O(maxD) system recursion stack space
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        depth(root, 0);
+
+        return LCA(root);
+    }
+};
+
+
+//Approach-2 (Using 1 Pass Solution)
 //T.C : O(n)
 //S.C : O(maxDepth) System stack space
 class Solution {
