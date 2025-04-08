ca35c2554d1427e13a6f4447e8cb4f68b7f0f949
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-04-09 01:08:34 +0530

    Update minimum-number-of-operations-to-make-elements-in-array-distinct.cpp

--- april/3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ april/3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,3 +1,32 @@
+//Approach-1 (Brute Force)
+//T.C : O(n^2)
+//S.C : O(n)
+class Solution {
+public:
+    int n;
+    bool check(vector<int>& nums, int start) {
+        unordered_set<int> st;
+        for (int j = start; j < n; j++) {
+            if (st.count(nums[j])) {
+                return false;
+            }
+            st.insert(nums[j]);
+        }
+        return true;
+    }
+    int minimumOperations(vector<int>& nums) {
+        n = nums.size();
+        int ops = 0;
+        for (int i = 0; i < n; i += 3) {
+            if (check(nums, i) == true) {
+                return ops;
+            }
+            ops++;
+        }
+        return ops;
+    }
+};
+
 
 //Approach-2 (Using reverse loop)
 //T.C : O(n)
