f75746074c0866502465be1afffeb475270807be
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-04-18 00:26:07 +0530

    Time: 2 ms (33.67%) | Memory: 16.9 MB (6.09%) - LeetSync

---
+++ april/2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
@@ -0,0 +1,45 @@
+
+//T.C : O(n * (log(k) + sqrt(k)))
+//S.C : O(n)
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int n = nums.size();
+        int result = 0;
+
+        unordered_map<int, vector<int>> indicesMap;
+        for(int i = 0; i < n; i++) {
+            indicesMap[nums[i]].push_back(i);
+        }
+
+        //k = 36
+        //1, 2, 3, 4, 5, 6
+        unordered_set<int> div;
+        for(int f = 1; f*f <= k; f++) { //O(sqrt(k))
+            if(k%f == 0) {
+                div.insert(f); //{1, 2, 3, 4, 6, 36, 18, 12, 9}
+                div.insert(k/f);
+            }
+        }
+
+        //O(n * (log(k) + sqrt(k)))
+        for(auto& [num, indices] : indicesMap) {
+            unordered_map<int, int> factorsMap;
+
+            for(int i : indices) {
+                int GCD = gcd(i, k); // T.C : log(k)
+                int j   = k / GCD;
+
+                result += factorsMap[j];
+
+                for(int f : div) {
+                    if(i % f == 0) {
+                        factorsMap[f]++;
+                    }
+                }
+            }
+        }
+        return result;
+        
+    }
+};
