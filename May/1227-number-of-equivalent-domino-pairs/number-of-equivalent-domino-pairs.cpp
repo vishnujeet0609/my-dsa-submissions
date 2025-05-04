03d3a6557b38ab63e699f630afb4851aae9afd57
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-05-04 21:22:25 +0530

    Update number-of-equivalent-domino-pairs.cpp

--- May/1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
+++ May/1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -1,3 +1,36 @@
+//Approach-1 (Using map of pair)
+//T.C : O(n * logn) , note that we are using ordered_map here and insertion in it is log(n)
+//S.C : O(n)
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        map<pair<int, int>, int> mp;
+
+        for(auto &d : dominoes) {
+            if(d[0] > d[1]) { //(2, 1) ---- (1, 2)
+                swap(d[0], d[1]);
+            }
+
+            mp[{d[0], d[1]}]++;
+        }
+
+        int result = 0;
+
+        for(auto &it : mp) {
+            int freq = it.second;
+
+            result += freq * (freq-1)/2; //freqC2
+        }
+
+        return result;
+
+    }
+};
+
+
+//Approach-2 (constant space)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
