03e134b7df31aa9744edea52c6b9358b44ebe26b
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-05-04 21:20:28 +0530

    Time: 4 ms (51.00%) | Memory: 26.4 MB (32.75%) - LeetSync

---
+++ May/1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        vector<int> vec(100);
+
+        int result = 0;
+
+        for(auto &d : dominoes) {
+            if(d[0] > d[1]) {
+                swap(d[0], d[1]);
+            }
+
+            int num = d[0]*10 + d[1]; //(a, b) --> a*10 + b;
+
+            result += vec[num];
+
+            vec[num]++;
+        }
+
+        return result;
+    }
+};
