ec06f9e5d4b8856a09dce0ae023ad92371aaf87a
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-05-05 22:02:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (94.11%) - LeetSync

---
+++ May/806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -0,0 +1,28 @@
+//Approach-1 (Recur + Memo)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int M = 1000000007;
+    int t[1001];
+    int solve(int n) {
+         if(n == 1 || n == 2)
+            return n;
+        if(n == 3)
+            return 5;
+        
+        if(t[n] != -1)
+            return t[n];
+        
+        return t[n] = (2*solve(n-1)%M + solve(n-3)%M)%M;
+    }
+    
+    int numTilings(int n) {
+       
+        memset(t, -1, sizeof(t));
+        
+        
+        return solve(n);
+        
+    }
+};
