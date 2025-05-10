b30546e41e6711fdf1a001b753ed85ddee394290
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-05-10 08:05:17 +0530

    Update count-number-of-balanced-permutations.cpp

--- May/3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.cpp
+++ May/3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.cpp
@@ -1,3 +1,32 @@
+//Approach-1 (Brute Force)
+//T.C : O(n * n!)
+//S.C : O(n * n!), storing all permutations in set
+class Solution {
+public:
+    int countBalancedPermutations(string num) {
+        sort(num.begin(), num.end()); //Sorting helps next_permutation to find all permutation, so we need to start from smallest (hence sorted)
+        unordered_set<string> seen;
+        int count = 0;
+
+        do {
+            if (seen.count(num)) 
+                continue;
+            seen.insert(num);
+            int evenSum = 0, oddSum = 0;
+            for (int i = 0; i < num.size(); ++i) {
+                int digit = num[i] - '0';
+                if (i % 2 == 0) evenSum += digit;
+                else oddSum += digit;
+            }
+            if (evenSum == oddSum) {
+                count++;
+            }
+        } while (next_permutation(num.begin(), num.end()));
+
+        return count;
+    }
+};
+
 
 //Approach-2 (Recursion + Memoization)
 //T.C : O(10 * n^2 * Sum)
