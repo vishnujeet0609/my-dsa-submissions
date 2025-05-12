56ab5fb385992f6e678a325048a8075cdc763190
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-05-12 21:53:05 +0530

    Update finding-3-digit-even-numbers.cpp

--- May/2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ May/2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -1,3 +1,34 @@
+//Approach-1 (Trying all possibilities using 3 for loops for 3 digits)
+//T.C : O(n^3 + SlogS), S = total 3 digits even numbers
+//S.C : O(S)
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        unordered_set<int> st;
+        int n = digits.size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                for(int k = 0; k < n; k++) {
+                    if(i == j || j == k || i == k) {
+                        continue;
+                    }
+
+                    int num = digits[i]*100 + digits[j] * 10 + digits[k];
+
+                    if(num >= 100 && num%2 == 0) {
+                        st.insert(num);
+                    }
+                }
+            }
+        }
+
+        vector<int> result(begin(st), end(st));
+        sort(begin(result), end(result));
+        return result;
+    }
+};
+
 
 //Approach-2 (Smartly finding valid digits for each position)
 //T.C : O(1)
