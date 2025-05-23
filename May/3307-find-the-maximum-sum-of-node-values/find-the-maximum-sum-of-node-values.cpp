3cce1631c32bfa6de205f5b38dcb804bb0e76755
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-05-23 21:51:10 +0530

    Update find-the-maximum-sum-of-node-values.cpp

--- May/3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ May/3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -1,3 +1,4 @@
+//Approach-1 (Greedily picking nodes to xor)
 //T.C : O(n)
 //S.C : O(1)
 class Solution {
@@ -27,3 +28,37 @@ public:
         return sum - minNukasan;
     }
 };
+
+
+//Approach-2(Greedy  + Sorting)
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+public:
+    typedef long long ll;
+    ll maximumValueSum(vector<int>& nums, int k,
+                              vector<vector<int>>& edges) {
+        vector<int> fayda;
+        ll normalSum = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            fayda.push_back((nums[i] ^ k) - nums[i]);
+            normalSum += 1ll * nums[i];
+        }
+        
+        sort(fayda.begin(), fayda.end(), greater<int>());
+        
+        for (int i = 0; i < fayda.size()-1; i += 2) {
+
+            ll pairSum = fayda[i] + fayda[i + 1];
+            
+            if (pairSum > 0) {
+                normalSum += pairSum;
+            }
+        }
+        return normalSum;
+    }
+};
+
+
+
