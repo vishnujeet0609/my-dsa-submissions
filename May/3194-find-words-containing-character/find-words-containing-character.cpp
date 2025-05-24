2776dd9d12d0783ee832646a4bf8b4c4770fa26b
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-05-24 22:21:07 +0530

    Time: 0 ms (100.00%) | Memory: 32.9 MB (80.27%) - LeetSync

---
+++ May/3194-find-words-containing-character/find-words-containing-character.cpp
@@ -0,0 +1,18 @@
+/*********************************************************** C++ **************************************************/
+//Approach (usin c++ STL)
+//T.C : O(m*n) 
+//S.C : O(1)
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> res;
+        int n = words.size();
+        for (int i = 0; i < n; ++i) {
+            if (words[i].find(x) != string::npos) {
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
+
