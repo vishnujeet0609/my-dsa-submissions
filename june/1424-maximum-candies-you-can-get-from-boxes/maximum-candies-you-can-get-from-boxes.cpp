99d1d80698ea428a185a4eb0701b036e4f3dfe38
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-06-03 21:08:56 +0530

    Update maximum-candies-you-can-get-from-boxes.cpp

--- june/1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
+++ june/1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
@@ -1,4 +1,4 @@
-
+//Approach-1 (Using DFS)
 //T.C : O(n), where n =  number of boxes, we don't visit any box more than once
 //S.C : O(n)
 class Solution {
@@ -48,3 +48,55 @@ public:
         return candiesCollected;
     }
 };
+
+
+//Approach-2 (Using BFS)
+//T.C : O(n), where n =  number of boxes, we don't visit any box more than once
+//S.C : O(n)
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        
+        int candiesCollected = 0;
+        unordered_set<int> visited;
+        unordered_set<int> foundBoxes;
+        //T.C : O(n) visiting all box only once , n = number of boxes
+        //S.C : O(n)
+
+        queue<int> que; //insert those which you have now and you can open it
+        for(int &box : initialBoxes) {
+            foundBoxes.insert(box);
+            if(status[box] == 1) {
+                que.push(box);
+                visited.insert(box);
+                candiesCollected += candies[box];
+            }
+        }
+
+        while(!que.empty()) {
+            int box = que.front();
+            que.pop();
+
+            for(int &insideBox : containedBoxes[box]) {
+                foundBoxes.insert(insideBox);
+                if(status[insideBox] == 1 && !visited.count(insideBox)) {
+                    que.push(insideBox);
+                    visited.insert(insideBox);
+                    candiesCollected += candies[insideBox];
+                }
+            }
+
+            for(int &boxKey : keys[box]) {
+                status[boxKey] = 1; //can be opened in future if we reach this box
+                if(foundBoxes.count(boxKey) && !visited.count(boxKey)) {
+                    que.push(boxKey);
+                    visited.insert(boxKey);
+                    candiesCollected += candies[boxKey];
+                }
+            }
+        }
+
+        return candiesCollected;
+    }
+};
+
