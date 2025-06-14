f86e2d66931e8fa72ea3fd3ed96aa3c69f3ebb4f
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-06-14 23:23:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (37.74%) - LeetSync

---
+++ june/2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -0,0 +1,35 @@
+//T.C : O(log10(n))
+//S.C : O(log10(n))
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string minNum = to_string(num);
+        string maxNum = to_string(num);
+        int n = minNum.length();
+
+        char ch = ' ';
+        for(int i = 0; i < n; i++) {
+            if(maxNum[i] != '9') {
+                ch = maxNum[i];
+                break;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(maxNum[i] == ch) {
+                maxNum[i] = '9';
+            }
+        }
+        
+
+        ch = minNum[0];
+        for(int i = 0; i < n; i++) {
+            if(minNum[i] == ch) {
+                minNum[i] = '0';
+            }
+        }
+
+        return stoi(maxNum) - stoi(minNum);
+
+    }
+};
