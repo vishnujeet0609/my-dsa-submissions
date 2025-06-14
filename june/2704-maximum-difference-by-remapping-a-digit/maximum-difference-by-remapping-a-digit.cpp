d716d21c2db103d0c8bbb7bb2a738309c571944e
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-06-14 23:25:43 +0530

    Update maximum-difference-by-remapping-a-digit.cpp

--- june/2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
+++ june/2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -1,3 +1,4 @@
+//Approach-1 (Simple iterating on digits)
 //T.C : O(log10(n))
 //S.C : O(log10(n))
 class Solution {
@@ -33,3 +34,27 @@ public:
 
     }
 };
+
+
+//Approach-2 (Using C++ STLs)
+//T.C : O(log10(n))
+//S.C : O(log10(n))
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string str1 = to_string(num);
+        string str2 = str1;
+
+        int idx = str1.find_first_not_of('9');
+        if(idx != string::npos) {
+            char ch = str1[idx];
+            replace(begin(str1), end(str1), ch, '9');
+        }
+
+        char ch = str2[0]; //given input will not have leading zero
+        replace(begin(str2), end(str2), ch, '0');
+
+        return stoi(str1) - stoi(str2);
+    }
+};
+
