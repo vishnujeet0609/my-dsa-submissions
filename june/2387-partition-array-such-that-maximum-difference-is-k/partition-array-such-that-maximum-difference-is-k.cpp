1793084e4d28746e70a7af40ea3de37eb315ec2a
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-06-19 22:17:54 +0530

    Time: 41 ms (19.38%) | Memory: 86.6 MB (100.00%) - LeetSync

---
+++ june/2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
@@ -0,0 +1,22 @@
+//Approach (Using sorting + greedily partitioning)
+//T.C : O(nlogn + n)
+//S.C : O(1)
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        sort(begin(nums), end(nums));
+        int n = nums.size();
+
+        int minVal = nums[0];
+        int count  = 1;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] - minVal > k) {
+                count++;
+                minVal = nums[i];
+            }
+        }
+
+        return count;
+    }
+};
