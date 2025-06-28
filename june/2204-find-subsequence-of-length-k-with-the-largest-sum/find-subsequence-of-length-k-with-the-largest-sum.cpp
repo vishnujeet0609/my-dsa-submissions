1dfa1c41e85cfc9c0a45cae09d0177d6a2e823a5
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-06-28 13:26:17 +0530

    Update find-subsequence-of-length-k-with-the-largest-sum.cpp

--- june/2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ june/2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,3 +1,4 @@
+a//Approach-1 (Using sorting with indices)
 //T.C : O(nlogn)
 //S.C : O(1)
 class Solution {
@@ -28,3 +29,42 @@ public:
         return result;
     }
 };
+
+
+//Approach-2 (Using nth_element for partial sort)
+//T.C : Average O(n+k)
+//S.C : O(1)
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        if (k == nums.size())
+            return nums;
+
+        // Copy nums to a temp vector to find k largest elements
+        vector<int> temp(nums);
+        
+        // Use nth_element to put the k largest elements in the first k positions (unordered)
+        nth_element(begin(temp), begin(temp) + k - 1, end(temp), greater<int>()); //Average TC is O(n)
+
+        // Determine the threshold value (k-th largest element)
+        int threshold = temp[k - 1];
+
+        // Count how many times threshold appears in top k elements
+        int countThreshold = count(temp.begin(), temp.begin() + k, threshold); //O(k)
+
+        vector<int> result;
+        for (int num : nums) {
+            if (num > threshold) {
+                result.push_back(num);
+            } else if (num == threshold && countThreshold > 0) {
+                result.push_back(num);
+                --countThreshold;
+            }
+            if (result.size() == k)
+                break;
+        }
+
+        return result;
+    }
+};
+
