028d2569bc3e97892af8ee0d2d2b5fc9c8bbc403
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-07-05 21:27:20 +0530

    Update find-lucky-integer-in-an-array.cpp

--- july/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
+++ july/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -1,3 +1,49 @@
+//Approach-1 (Using fixed size array to store frequency)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int, int> mp;
+        
+        for(int &x : arr) {
+            mp[x]++;
+        }
+        
+        int ans = -1;
+        for(auto &it : mp) {
+            if(it.first == it.second)
+                ans = max(ans, it.first);
+        }
+        
+        return ans;
+    }
+};
+
+
+//Approach-2 (Using fixed size array to store frequency)
+//T.C : O(n)
+//S.C : O(501) ~ O(1)
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        int freq[501] = {0};
+
+        for(int &num : arr) {
+            freq[num]++;
+        }
+
+        for(int i = 500; i >= 1; i--) {
+            if(freq[i] == i)
+                return i;
+        }
+
+        return -1;
+    }
+};
+
+
+//Approach-3 (Using bits to store frequency)
 //T.C : O(n)
 //S.C : O(1) in place
 class Solution {
