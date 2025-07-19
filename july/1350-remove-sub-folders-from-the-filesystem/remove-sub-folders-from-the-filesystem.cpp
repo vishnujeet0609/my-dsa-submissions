88a0241096b52f2ec4b9555dc63121c9adc921d3
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-07-19 22:37:54 +0530

    Update remove-sub-folders-from-the-filesystem.cpp

--- july/1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ july/1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,3 +1,39 @@
+//Approach-1 (Using set and substring find)
+//T.C : O(n*L^2)
+//S.C : O(n) //You can consider the length of each character as well - O(n*L)
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        unordered_set<string> st(begin(folder), end(folder));
+        vector<string> result;
+
+        for(string& currFolder : folder) {
+            bool isSubFolder = false;
+            string tempFolder = currFolder;
+            while(!currFolder.empty()) {
+                size_t position = currFolder.find_last_of('/');
+
+                currFolder = currFolder.substr(0, position);
+
+                if(st.find(currFolder) != st.end()) {
+                    //it means currFolder is a sub-folder
+                    isSubFolder = true;
+                    break;
+                }
+            }
+
+            if(!isSubFolder) {
+                result.push_back(tempFolder);
+            }
+
+        }
+
+        return result;
+    }
+};
+
+
+//Approach-2 (Using Sorting)
 //T.C : O(n*logn) //You can consider the length of each character as well - O(n*L*logn)
 //S.C : O(1)
 class Solution {
