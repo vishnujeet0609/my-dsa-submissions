37cb7a002d9f974105679f38a52af18d52f87e16
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2025-09-16 00:42:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (69.10%) - LeetSync

---
+++ August/1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,30 @@
+//T.C : O(m+n), m = length of text, n = length of brokenLetters
+//S.C : O(1)
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        int n = text.length();
+
+        bool mp[26];
+        for(char &ch : brokenLetters) {
+            mp[ch-'a'] = true;
+        }
+
+        int result        = 0;
+        bool foundBroken  = false;
+        for(char &ch : text) {
+            if(ch == ' ') {
+                if(!foundBroken) {
+                    result++;
+                }
+                foundBroken = false;
+            } else if (mp[ch - 'a']) {
+                foundBroken = true; //found
+            }
+        }
+        if(foundBroken == 0)
+            result++;
+
+        return result;
+    }
+};
