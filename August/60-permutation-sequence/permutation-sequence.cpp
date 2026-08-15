18b9208e65759ce815bfb71db4ec0ff651a59f44
vishnujeet0609 <102174022+vishnujeet0609@users.noreply.github.com>
2026-08-15 20:10:01 +0530

    Time: 3 ms (25.96%) | Memory: 9.3 MB (7.26%) - LeetSync

--- August/60-permutation-sequence/permutation-sequence.cpp
+++ August/60-permutation-sequence/permutation-sequence.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
+    vector<int>fact,arr;
+    void solve(string &ans, int n, int k) {
+      if(n==1){
+        ans+=to_string(arr.back());
+        return;
+      }
 
-    void nextPermutation(vector<int>& nums) {
-        int n = nums.size(),k,l;
-        for(k=n-2;k>=0;k--){
-            if(nums[k]<nums[k+1])break;
-        }
-        if(k<0){
-            reverse(nums.begin(),nums.end());
-        } else{
-        for(l=n-1;l>=k+1;l--){
-            if(nums[l]>nums[k])break;
-        }
-        
-        swap(nums[k],nums[l]);
-        reverse(nums.begin()+k+1,nums.end());
-        }
+      int blockSize = fact[n-1];
+      int index = k/blockSize;
+      if(k % blockSize == 0){
+        index--;
+      }
+      ans+=to_string(arr[index]);
+      arr.erase(arr.begin() + index);
+
+      k-=(blockSize * index);
+      solve(ans, n-1, k);
     }
 
 
     string getPermutation(int n, int k) {
-        vector<int>arr;
+        fact.push_back(1);
+        int f = 1;
+        for(int i =1;i<=n;i++){
+            f*=i;
+            fact.push_back(f);
+        }
         for(int i=1;i<=n;i++){
             arr.push_back(i);
         }
-        for(int i=1; i<k;i++){
-            nextPermutation(arr);
-        }
-        string s = "";
-
-        for(int i=0;i<n;i++){
-            int digit = arr[i];
-            s+=to_string(digit);
-        }
-        return s;
+        string ans = "";
+        solve(ans, n, k);
+        return ans;
+        
     }
 };
